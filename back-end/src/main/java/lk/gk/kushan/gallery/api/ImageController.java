8aba2719627b5f562640d38ca310c68f253700b1
Kushan Weerarathne <kushanweerarathne14@gmail.com>
2023-07-01 17:45:04 +0530

    Initialized the back-end configuration classes
       -Expose ImageController web service and completed its implementation

--- back-end/src/main/java/lk/gk/kushan/gallery/api/ImageController.java
+++ back-end/src/main/java/lk/gk/kushan/gallery/api/ImageController.java
@@ -5,18 +5,37 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin
 @RequestMapping("/images")
 public class ImageController {
+
     @Autowired
     private ServletContext servletContext;
 
     @GetMapping
-    public String getAllImages() {
-        return "<h1>Get All Images</h1>";
+    public List<String> getAllImages(UriComponentsBuilder uriBuilder) {
+        ArrayList<String> imageFileList = new ArrayList<>();
+        String imgDirPath = servletContext.getRealPath("/images");
+        File imgDir = new File(imgDirPath);
+        String[] imageFileNames = imgDir.list();
+        for (String imageFileName : imageFileNames) {
+            UriComponentsBuilder clone = uriBuilder.cloneBuilder();
+            String url = clone.pathSegment("images",imageFileName).build().toUriString();
+            imageFileList.add(url);
+        }
+        return imageFileList;
     }
+
+
+
+
 }
