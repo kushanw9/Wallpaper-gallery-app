6afd03c533ac96b8f1dbc6de986a34e96759cc5a
Kushan Weerarathne <kushanweerarathne14@gmail.com>
2023-07-02 09:54:55 +0530

    Completed loadAllImages()- front-end and ImageController web service

--- back-end/src/main/java/lk/gk/kushan/gallery/api/ImageController.java
+++ back-end/src/main/java/lk/gk/kushan/gallery/api/ImageController.java
@@ -1,14 +1,16 @@
 package lk.gk.kushan.gallery.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Part;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -34,8 +36,5 @@ public class ImageController {
         }
         return imageFileList;
     }
-
-
-
 
 }
--- front-end/src/js/main.js
+++ front-end/src/js/main.js
@@ -16,51 +16,10 @@ $(document).on('keydown', (evt) => {
         overlay.addClass('d-none');
     }
 });
-overlay.on('dragover', (evt) => evt.preventDefault());
-overlay.on('drop', (evt) => evt.preventDefault());
-dropZoneElm.on('dragover', (evt) => {
-    evt.preventDefault();
-});
-dropZoneElm.on('drop', (evt) => {
-    evt.preventDefault();
-    const droppedFiles = evt.originalEvent
-        .dataTransfer.files;
-    const imageFiles = Array.from(droppedFiles)
-        .filter(file => file.type.startsWith("image/"));
-    if (!imageFiles.length) return;
-    overlay.addClass("d-none");
-    uploadImages(imageFiles);
-});
-mainElm.on('click', '.image:not(.loader)', (evt)=> {
-    evt.target.requestFullscreen();
-});
 
-function uploadImages(imageFiles){
-    const formData = new FormData();
-    imageFiles.forEach(imageFile => {
-        const divElm = $(`<div class="image loader"></div>`);
-        divElm.append(cssLoaderHtml);
-        mainElm.append(divElm);
 
-        formData.append("images", imageFile);
-    });
-    const jqxhr = $.ajax(`${REST_API_URL}/images`, {
-        method: 'POST',
-        data: formData,
-        contentType: false,         // by default jQuery uses application/x-www-form-urlencoded
-        processData: false          // by default jQuery tries to convert the data into String
-    });
 
-    jqxhr.done((imageUrlList)=> {
-        imageUrlList.forEach(imageUrl => {
-            const divElm = $(".image.loader").first();
-            divElm.css('background-image', `url('${imageUrl}')`);
-            divElm.empty();
-            divElm.removeClass('loader');
-        });
-    });
-    jqxhr.always(()=> $(".image.loader").remove());
-}
+
 
 function loadAllImages() {
     const jqxhr = $.ajax(`${REST_API_URL}/images`);
